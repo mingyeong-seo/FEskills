f232bcbc3cb15c6272b91dd4e7dcac4ecde30ecf
ming03-dev <mingyeongs03@naver.com>
2026-01-28 10:06:57 +0900

    2026-01-28 | Score 과제 업로드

--- java_class/src/javaFund/lecture/Score.java
+++ java_class/src/javaFund/lecture/Score.java
@@ -1,4 +1,5 @@
 package javaFund.lecture;
+
 import java.util.Scanner;
 
 public class Score {
@@ -14,23 +15,95 @@ public class Score {
 		 * 모든 점수는 배열로 관리되어야 합니다. 총점도 학점은 90점 이상은 A, 80점 이상은 B, 70점 이상은 C, 나머지는 F처리하는데
 		 * switch로 작성하세요
 		 */
-		
+
 		// 점수 입력 받기
 		boolean isScore = false;
-		
-		while(!isScore) {
-			System.out.println("국어 점수입력");
-			Scanner sc = new Scanner(System.in);
-			
-			int score = sc.nextInt();
+
+		int total = 0;
+		int avg = 0;
+
+		int score[] = new int[3];
+		char grade = ' ';
+
+		Scanner input = new Scanner(System.in);
+
+		while (!isScore) {
+			while (true) {
+				System.out.print("국어 점수입력(0~100점): ");
+
+				if (input.hasNextInt()) {
+					score[0] = input.nextInt();
+
+					if (score[0] < 0 || score[0] > 100) {
+						System.out.println("0~100 사이로 입력하세요.");
+						continue;
+					}
+					break;
+				} else {
+					input.next();
+					System.out.println("정수로 입력하세요.");
+				}
+			}
+
+			while (true) {
+				System.out.print("수학 점수입력(0~100점): ");
+
+				if (input.hasNextInt()) {
+					score[1] = input.nextInt();
+
+					if (score[1] < 0 || score[1] > 100) {
+						System.out.println("0~100 사이로 입력하세요.");
+						continue;
+					}
+					break;
+				} else {
+					input.next();
+					System.out.println("정수로 입력하세요.");
+				}
+			}
+
+			while (true) {
+				System.out.print("영어 점수입력(0~100점): ");
+
+				if (input.hasNextInt()) {
+					score[2] = input.nextInt();
+
+					if (score[2] < 0 || score[2] > 100) {
+						System.out.println("0~100 사이로 입력하세요.");
+						continue;
+					}
+					break;
+				} else {
+					input.next();
+					System.out.println("정수로 입력하세요.");
+				}
+			}
+
+			isScore = true;
 		}
-		
-		
-		
-		
-		
-		
 
+		for (int i = 0; i < score.length; i++) {
+			total += score[i];
+		}
+
+		avg = total / 3;
+
+		switch (avg / 10) {
+		case 10:
+		case 9:
+			grade = 'A';
+			break;
+		case 8:
+			grade = 'B';
+			break;
+		case 7:
+			grade = 'C';
+			break;
+		default:
+			grade = 'F';
+		}
+
+		System.out.println("총점은 " + total + ", 평균은 " + avg + ", 학점은 " + grade + "입니다.");
+		input.close();
 	}
-
 }
