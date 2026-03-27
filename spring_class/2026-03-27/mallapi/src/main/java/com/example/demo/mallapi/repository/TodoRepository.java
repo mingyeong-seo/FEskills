c386b09543c0d04242e722c3c811d1f4c9ed7ae3
서민경 <mingyeongs03@naver.com>
2026-03-27 18:05:14 +0900

    Delete spring_class/2026-03-27/mallapi/src/main/java/com/example/demo/mallapi/repository/TodoRepository.java

--- spring_class/2026-03-27/mallapi/src/main/java/com/example/demo/mallapi/repository/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.mallapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.mallapi.domain.Todo;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-
-}
